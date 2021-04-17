ac020f22b23f149ef50125863c50c5611a6daf25
Daniel Jonathan Zurita Mena <dzurita@cifpfbmoll.eu>
2021-04-17 15:48:17 +0200

    feat(filters): Create filters component

---
+++ pages/_filters.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+import { Container, Button } from "reactstrap";
+
+export default function SneakerFilters() {
+  return (
+    <Container className="filters" fluid>
+      <Button className="filter" color="dark">
+        Raffle
+      </Button>
+      <Button className="filter" color="dark">
+        Raffle
+      </Button>
+      <Button className="filter" color="dark">
+        Raffle
+      </Button>
+      <Button className="filter" color="dark">
+        Raffle
+      </Button>
+      <Button className="filter" color="dark">
+        Raffle
+      </Button>
+    </Container>
+  );
+}
